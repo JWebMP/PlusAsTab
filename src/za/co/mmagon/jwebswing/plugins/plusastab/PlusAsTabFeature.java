7a5624daeeaea506c6105e1a9bfa77440a7e5a2c
Marc Magon <ged_marc@hotmail.com>
2017-03-04 09:24:24 +0200

    v0.9

--- src/za/co/mmagon/jwebswing/plugins/plusastab/PlusAsTabFeature.java
+++ src/za/co/mmagon/jwebswing/plugins/plusastab/PlusAsTabFeature.java
@@ -1,10 +1,10 @@
-/*
- * Copyright (C) 2017 GedMarc
+/* 
+ * Copyright (C) 2017 Marc Magon
  *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -12,8 +12,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package za.co.mmagon.jwebswing.plugins.plusastab;
 
@@ -21,6 +20,7 @@ import za.co.mmagon.jwebswing.Feature;
 import za.co.mmagon.jwebswing.base.ComponentHierarchyBase;
 import za.co.mmagon.jwebswing.base.references.JavascriptReference;
 import za.co.mmagon.jwebswing.htmlbuilder.javascript.JavaScriptPart;
+import za.co.mmagon.jwebswing.plugins.ComponentInformation;
 
 /**
  *
@@ -28,6 +28,7 @@ import za.co.mmagon.jwebswing.htmlbuilder.javascript.JavaScriptPart;
  * @since 07 Feb 2017
  *
  */
+@ComponentInformation(name = "Plus As Tab", description = "Allows you to automatically move to the next input field based on a key press", url = "https://github.com/joelpurra/plusastab")
 public class PlusAsTabFeature extends Feature<JavaScriptPart, Feature> implements IPlusAsTab
 {
 
