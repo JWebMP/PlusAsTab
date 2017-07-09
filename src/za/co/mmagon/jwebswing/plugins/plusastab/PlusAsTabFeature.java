7874d64c809d8f4d8cd98b83c60c1b2083fb34de
Marc Magon <ged_marc@hotmail.com>
2017-07-09 05:05:15 +0200

    V 0.12 updates, complete switch to fast classpath scanner, threaded implementation

--- src/za/co/mmagon/jwebswing/plugins/plusastab/PlusAsTabFeature.java
+++ src/za/co/mmagon/jwebswing/plugins/plusastab/PlusAsTabFeature.java
@@ -1,4 +1,4 @@
-/* 
+/*
  * Copyright (C) 2017 Marc Magon
  *
  * This program is free software: you can redistribute it and/or modify
@@ -29,7 +29,8 @@ import za.co.mmagon.jwebswing.plugins.ComponentInformation;
  *
  */
 @ComponentInformation(name = "Plus As Tab", description = "Allows you to automatically move to the next input field based on a key press", url = "https://github.com/joelpurra/plusastab")
-public class PlusAsTabFeature extends Feature<JavaScriptPart, Feature> implements IPlusAsTab
+public class PlusAsTabFeature extends Feature<JavaScriptPart, PlusAsTabFeature>
+        implements IPlusAsTab
 {
 
     private static final long serialVersionUID = 1L;
