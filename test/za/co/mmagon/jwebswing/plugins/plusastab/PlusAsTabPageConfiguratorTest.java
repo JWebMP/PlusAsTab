15c2933e6838423f242a86d5079d32415e217d03
Marc Magon <ged_marc@hotmail.com>
2017-10-16 21:20:59 +0200

    Updates and preparation for .12
    Signed-off-by: Marc Magon <ged_marc@hotmail.com>

--- test/za/co/mmagon/jwebswing/plugins/plusastab/PlusAsTabPageConfiguratorTest.java
+++ test/za/co/mmagon/jwebswing/plugins/plusastab/PlusAsTabPageConfiguratorTest.java
@@ -2,7 +2,7 @@ package za.co.mmagon.jwebswing.plugins.plusastab;
 
 
 import org.junit.jupiter.api.Test;
-import za.co.mmagon.BaseTestClass;
+import za.co.mmagon.jwebswing.BaseTestClass;
 import za.co.mmagon.jwebswing.Page;
 import za.co.mmagon.jwebswing.generics.WebReference;
 
