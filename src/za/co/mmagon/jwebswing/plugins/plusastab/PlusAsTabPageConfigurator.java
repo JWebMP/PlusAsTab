c59c0816e974089e6675040a3c227e7591dfc486
Marc Magon <ged_marc@hotmail.com>
2018-01-05 00:53:10 +0200

    movement out of bower

--- src/za/co/mmagon/jwebswing/plugins/plusastab/PlusAsTabPageConfigurator.java
+++ src/za/co/mmagon/jwebswing/plugins/plusastab/PlusAsTabPageConfigurator.java
@@ -46,8 +46,8 @@ public class PlusAsTabPageConfigurator extends PageConfigurator
 
 	private static final long serialVersionUID = 1L;
 
-	private JavascriptReference jsReference = new JavascriptReference("EmulateTab", 1.0, "bower_components/emulatetab.joelpurra/emulateTab.min.js");
-	private JavascriptReference jsReferenceActual = new JavascriptReference("PlusAsTab", 1.0, "bower_components/jquery-plusastab/src/plusastab.joelpurra.min.js");
+	private JavascriptReference jsReference = new JavascriptReference("EmulateTab", 1.0, "emulatetab.joelpurra/emulateTab.min.js");
+	private JavascriptReference jsReferenceActual = new JavascriptReference("PlusAsTab", 1.0, "jquery-plusastab/src/plusastab.joelpurra.min.js");
 
 	/**
 	 * Sets the component to be included in the plus as tab option
